9f28742f1b8806b38480c05bfe2d75d036bf7cee
Vitor Oliveira <vitor.oliveira@sankhya.com.br>
2025-02-19 22:59:38 -0300

    Configuração inicial do projeto PIX API

---
+++ src/test/java/br/com/itsolution/fintech/pix_api/PixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itsolution.fintech.pix_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
