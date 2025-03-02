b9b51ac0684eb3d897ad54cdb1cd8434fa9a8c34
Vitor Oliveira <vitor.oliveira@sankhya.com.br>
2025-03-02 19:20:01 -0300

    Adicionando config de para o banco do brasil e o README.md do projeto.

--- src/main/java/br/com/itsolution/fintech/pix_api/dto/AutenticacaoResponseDTO.java
+++ src/main/java/br/com/itsolution/fintech/pix_api/dto/AutenticacaoResponseDTO.java
@@ -2,11 +2,20 @@ package br.com.itsolution.fintech.pix_api.dto;
 
 public class AutenticacaoResponseDTO {
     private String banco;
-    private String token;
+    private String access_token;
+    private String token_type;
+    private long expires_in;
+
+    public AutenticacaoResponseDTO() {
+
+    }
 
     public AutenticacaoResponseDTO(String banco, String token) {
-        this.banco = banco;
-        this.token = token;
+        this.banco        = banco;
+        this.access_token = access_token;
+        this.token_type   = token_type;
+        this.expires_in   = expires_in;
+
     }
 
     public String getBanco() {
@@ -17,11 +26,28 @@ public class AutenticacaoResponseDTO {
         this.banco = banco;
     }
 
-    public String getToken() {
-        return token;
+    public String getAccess_token() {
+        return access_token;
     }
 
-    public void setToken(String token) {
-        this.token = token;
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
     }
+
+    public String getToken_type() {
+        return token_type;
+    }
+
+    public void setToken_type(String token_type) {
+        this.token_type = token_type;
+    }
+
+    public long getExpires_in() {
+        return expires_in;
+    }
+
+    public void setExpires_in(long expires_in) {
+        this.expires_in = expires_in;
+    }
+
 }
